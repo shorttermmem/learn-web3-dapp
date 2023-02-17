6a76cfe41b4809d49e8ae9eaa7312a8ce833e480
global user <user@github.com>
2023-02-16 20:29:53 -0500

    add smart contract test

--- contracts/polygon/SimpleStorage/test/TestSimpleStorage.sol
+++ contracts/polygon/SimpleStorage/test/TestSimpleStorage.sol
@@ -11,17 +11,13 @@ contract TestSimpleStorage {
 
   function testInitialStoredDataUsingDeployedContract() public {
     SimpleStorage store = SimpleStorage(DeployedAddresses.SimpleStorage());
-
     Assert.equal(store.get(), 0, "storedData should be 0 initially");
   }
 
   function testSettingStoredData() public {
     SimpleStorage store = new SimpleStorage();
-
     uint expected = 10000;
-
     store.set(expected);
-
     Assert.equal(store.get(), expected, "storedData should equal 10000");
   }
 
