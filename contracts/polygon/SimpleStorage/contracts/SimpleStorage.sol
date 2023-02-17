05e86e63a92da4eeef597b3ec2d0db729a5ec23c
global user <user@github.com>
2023-02-16 19:55:50 -0500

    impl first smart contract and installed truffle

--- contracts/polygon/SimpleStorage/contracts/SimpleStorage.sol
+++ contracts/polygon/SimpleStorage/contracts/SimpleStorage.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0;
 contract SimpleStorage {
     uint storedData;
 
+    // call once
     constructor() {
   		  storedData = 0;
   	}
 
+    // public apis
     function set(uint x) public {
         storedData = x;
     }
