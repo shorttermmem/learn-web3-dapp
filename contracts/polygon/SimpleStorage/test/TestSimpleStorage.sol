82fe79dfe88bdeaa5d35af0684bcb0624a64d0bd
global user <user@github.com>
2023-02-16 21:19:33 -0500

    add smart contract test

--- contracts/polygon/SimpleStorage/test/TestSimpleStorage.sol
+++ contracts/polygon/SimpleStorage/test/TestSimpleStorage.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.0;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
+
+// tested contract
 import "../contracts/SimpleStorage.sol";
 
 contract TestSimpleStorage {
