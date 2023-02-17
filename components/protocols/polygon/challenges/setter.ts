9518b319cd01756d9989d419cccebf340df4725d
global user <user@github.com>
2023-02-16 20:55:03 -0500

    test set op in smart contract

--- components/protocols/polygon/challenges/setter.ts
+++ components/protocols/polygon/challenges/setter.ts
@@ -9,10 +9,17 @@ const setValue = async (contractAddress: string, value: number) => {
   try {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
-    // try to figure out the expected parameters
-    const contract = new ethers.Contract(undefined);
-    // try to figure out the expected method
-    const transactionResult = undefined;
+
+    // contract address is 0xe85D60aa947Aee5e794674413a05607B49bdDc80
+    const contract = new ethers.Contract(
+      contractAddress,
+      SimpleStorageJson.abi,
+      signer,
+    );
+
+    console.log(provider.getCode(contractAddress));
+    // add gasLimit to prevent transaction reverting.
+    const transactionResult = await contract.set(value, {gasLimit: 500000});
     const receipt = await transactionResult.wait();
     return {hash: receipt.transactionHash};
   } catch (error) {
